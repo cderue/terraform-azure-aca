3dbba595b5874094f9d1ac7d0cd5ba037c64ca6d
Cédric Derue <cedric.derue@gmail.com>
2025-02-17 11:45:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "location" {
   description = "Azure infrastructure region"
   type    = string
-  default = "westeurope"
 }
 
 variable "app" {
   description = "Application that we want to deploy"
   type    = string
-  default = "hashitalks"
 }
 
 variable "image" {
   description = "Docker image used for deployment"
   type    = string
-  default = "hashitalks"
 }
 
 variable "env" {
   description = "Application env"
   type    = string
-  default = "dev"
 }
